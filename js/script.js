39218d04a00d53828951ef63e80acd893824798a
LorenzoEmanuele00 <lorenzoema2800@gmail.com>
2023-11-06 15:27:53 +0100

    add image and adjust layout

--- js/script.js
+++ js/script.js
@@ -2,32 +2,32 @@ const teamMembers = [
     {
         name: `Wayne Barnett`,
         role: `Founder & CEO`,
-        photo: `wayne-barnett-founder-ceo.jpg`,
+        photo: "wayne-barnett-founder-ceo.jpg",
     },
     {
         name: `Angela Caroll`,
         role: `Chief Editor`,
-        photo: `angela-caroll-chief-editor.jpg`,
+        photo: "angela-caroll-chief-editor.jpg",
     },
     {
         name: `Walter Gordon`,
         role: `Office Manager`,
-        photo: `walter-gordon-office-manager.jpg`,
+        photo: "walter-gordon-office-manager.jpg",
     },
     {
         name: `Angela Lopez`,
         role: `Social Media Manager`,
-        photo: `angela-lopez-social-media-manager.jpg`,
+        photo: "angela-lopez-social-media-manager.jpg",
     },
     {
         name: `Scott Estrada`,
         role: `Developer`,
-        photo: `scott-estrada-developer.jpg`,
+        photo: "scott-estrada-developer.jpg",
     },
     {
         name: `Barbara Ramos`,
         role: `Graphic Designer`,
-        photo: `barbara-ramos-graphic-designer.jpg`,
+        photo: "barbara-ramos-graphic-designer.jpg",
     },
 ];
 
@@ -43,8 +43,8 @@ for (let i = 0; i < teamMembers.length; i++) {
     text += `
         <div class = "col">
             <div class = "card rounded-4">
-                <div>
-                    <p>${teamMembers[i].photo}<p>
+                <img class = "rounded-top-4" src = "../img/${teamMembers[i].photo}">
+                <div class = "py-3">
                     <h3>${teamMembers[i].name}</h3>
                     <h5>${teamMembers[i].role}</h5> 
                 </div>
