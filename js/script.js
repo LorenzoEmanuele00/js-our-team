a106c78959d6b035cec4c145e46798eb9b5a81a6
LorenzoEmanuele00 <lorenzoema2800@gmail.com>
2023-11-06 14:47:20 +0100

    console log for every information

--- js/script.js
+++ js/script.js
@@ -1,34 +1,38 @@
 const teamMembers = [
     {
         name: `Wayne Barnett`,
-        position: `Founder & CEO`,
+        role: `Founder & CEO`,
         photo: `wayne-barnett-founder-ceo.jpg`,
     },
     {
         name: `Angela Caroll`,
-        position: `Chief Editor`,
+        role: `Chief Editor`,
         photo: `angela-caroll-chief-editor.jpg`,
     },
     {
         name: `Walter Gordon`,
-        position: `Office Manager`,
+        role: `Office Manager`,
         photo: `walter-gordon-office-manager.jpg`,
     },
     {
         name: `Angela Lopez`,
-        position: `Social Media Manager`,
+        role: `Social Media Manager`,
         photo: `angela-lopez-social-media-manager.jpg`,
     },
     {
         name: `Scott Estrada`,
-        position: `Developer`,
+        role: `Developer`,
         photo: `scott-estrada-developer.jpg`,
     },
     {
         name: `Barbara Ramos`,
-        position: `Graphic Designer`,
+        role: `Graphic Designer`,
         photo: `barbara-ramos-graphic-designer.jpg`,
     },
 ]
 
-console.log(teamMembers)
+for (let i = 0; i < teamMembers.length; i++) {
+    console.log(teamMembers[i].name);
+    console.log(teamMembers[i].role);
+    console.log(teamMembers[i].photo);
+  }
